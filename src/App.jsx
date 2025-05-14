26cd33ef2705c42f5fc0c2a7a1b53e39cb093140
DevM <dev.mukeshcgs@gmail.com>
2025-05-14 16:29:24 +0530

    smooth scroll plug in added to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,30 @@ import * as Sentry from '@sentry/react';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import TimelineExmp from './components/TimelineExmp';
+import ScrollTriggerExmp from './components/ScrollTriggerExmp';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+
 const App = () => {
   return (
-    <main className="bg-white">
-      <Navbar />
-      <section className='h-screen bg-sky-500'></section>
-      {/* <Hero /> */}
-      {/* <Features/> */}
-      <section className='h-screen bg-red-500'></section>
-      <TimelineExmp/>
-      <section className='h-screen bg-yellow-500'></section>
-      <section className='h-screen'></section>
-      <Footer />
+    <main className="bg-white" id="smooth-wrapper">
+      <div id="smooth-content">
+        <Navbar />
+        <section className='h-screen bg-sky-500'></section>
+        <ScrollTriggerExmp />
+        <section className='h-screen bg-green-500'></section>
+        {/* <Hero /> */}
+        {/* <Features/> */}
+        <section className='h-screen bg-red-500'></section>
+        <TimelineExmp />
+        <section className='h-screen bg-yellow-500'></section>
+        <section className='h-screen'></section>
+        <Footer />
+      </div>
     </main>
   )
 }
